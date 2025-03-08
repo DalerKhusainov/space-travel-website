fd4d0dd73b3b5a115851c1b50f661fd64c0c325d
DalerKhusainov <dalerkhusainov85@gmail.com>
2025-03-08 20:55:34 +0500

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div>
-      Hello
+    <div className="container grid">
+      <div className="box bg-dark">a</div>
+      <div className="box">b</div>
+      <div className="box">c</div>
+      <div className="box">d</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
