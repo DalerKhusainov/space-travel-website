635047b26bcc558dc286550fa72d6d058c408aa3
DalerKhusainov <dalerkhusainov85@gmail.com>
2025-03-09 23:30:24 +0500

    almost finished custom utility classes for typography

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="container grid">
-      <div className="box bg-dark">a</div>
-      <div className="box">b</div>
-      <div className="box">c</div>
-      <div className="box">d</div>
+      <h1 className="">Hello World</h1>
     </div>
   );
 }
